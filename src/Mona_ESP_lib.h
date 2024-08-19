71c99c8cf56d48e8ee94146922ac631570b3ec37
ICE9-Liam <liam@ice9robotics.co.uk>
2024-08-19 14:31:17 +0100

    pin update

--- src/Mona_ESP_lib.h
+++ src/Mona_ESP_lib.h
@@ -42,10 +42,10 @@
 //To move a motor the selected direction has to be set HIGH and the
 //contrary direction low. Example: For right motor forward set:
 // Mot_right_forward=HIGH and Mot_right_backward=LOW
-#define Mot_right_forward         21 //19
-#define Mot_right_backward        19 //21
-#define Mot_left_forward          18  //4
-#define Mot_left_backward         4 // 18
+#define Mot_right_forward         19 //19
+#define Mot_right_backward        21 //21
+#define Mot_left_forward          4  //4
+#define Mot_left_backward         18 // 18
 
 //Motor Feedback
 #define Mot_right_feedback	      39 //Pulses read from right motor encoder
